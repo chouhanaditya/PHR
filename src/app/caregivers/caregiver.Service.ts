50a71d89f3cc9aea015636efa7fd746d81ae63bc
Aditya Chouhan <aditya.chouhan@unmc.edu>
2017-07-06 13:45:15 -0500

    Caregivers version 4

--- src/app/caregivers/caregiver.Service.ts
+++ src/app/caregivers/caregiver.Service.ts
@@ -3,9 +3,9 @@ import {caregiver} from "./caregiver.Model";
 export class caregiverService{
 
   caregiversList : caregiver[] = [
-    new caregiver( 1, "Tom Windle", "","Tom.Windle@unmc.edu","(402) 987- 3241","Friend",false,true,true,true,true,false,true,false,true,false,true,false,true,false,true),
-    new caregiver( 2, "Yves", "","yves@unomaha.edu","(402) 458- 7412","Son",false,true,true,true,true,false,true,false,true,false,true,false,true,false,true),
-    new caregiver( 3, "Aditya", "","achouhan@unomaha.edu","(402) 913- 4882","Father",false,true,true,true,true,false,true,false,true,false,true,false,true,false,true),
+    new caregiver( 1, "Tom", "","Tom.Windle@unmc.edu","(402) 987- 3241","Friend",false,true,true,true,true,true,true,false,true,false,true,true,true,false,true),
+    new caregiver( 2, "Yves", "","yves@unomaha.edu","(402) 458- 7412","Son",false,true,true,true,true,false,true,true,true,false,true,true,true,true,true),
+    new caregiver( 3, "Aditya", "","achouhan@unomaha.edu","(402) 913- 4882","Father",false,true,true,true,true,true,true,false,true,false,true,true,true,true,true),
     ];
 
     caregiverGeneralDescription : string =  "Caregivers are the individuals which help patients with the basic life functions, manage medical care, help people  maintain health and remain in their home or community. " +
